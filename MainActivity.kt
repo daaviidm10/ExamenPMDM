8b33b87f61bde29d57c4811e877e2caaf5ad507d
daaviidm10 <72596994+daaviidm10@users.noreply.github.com>
2021-12-13 10:59:10 +0100

    Examen PMDM
    commits

---
+++ MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.examenpmdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    public override fun onResume() {
+
+        super.onResume();
+
+    }
+    public fun cuentaDec(){
+        for (num in 100..0){
+            Log.i("Cuenta decreciente desde 100 a 0", num.toString())
+        }
+        val bToast: Button =findViewById(R.id.botonDecre)
+        bToast.setOnClickListener{
+            Toast.makeText(this.applicationContext, "Mensaje del Toast", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
